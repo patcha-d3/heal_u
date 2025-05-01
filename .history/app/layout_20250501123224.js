a0ff636d1ed0621ddaaa376b58ec6dabd93e18fd
patcha-d3 <psricome@my.bcit.ca>
2025-05-01 13:30:58 -0700

    Remove heal_u redundant readme

--- .history/app/layout_20250501123224.js
+++ .history/app/layout_20250501123224.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Bottomnav from "./ui/Bottomnav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
