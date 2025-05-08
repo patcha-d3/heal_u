3b5cccab79b753d39f61999883c9d5369ac9cf7e
patcha-d3 <psricome@my.bcit.ca>
2025-05-08 15:52:04 -0700

    Added accordion component

--- app/home/page.js
+++ app/home/page.js
@@ -1,3 +1,29 @@
+import Accordion from "../../ui/Accordion/Accordion";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  const accordionItems = [
+    {
+      title: "Neck",
+      subitems: [
+        { day: 1, label: "Neck Rotations", status: "done" },
+        { day: 2, label: "Neck Stretch", status: "active" },
+        { day: 3, label: "Side-to-Side Neck Stretch", status: "pending" },
+      ],
+    },
+    {
+      title: "Lower Back",
+      subitems: [
+        { day: 1, label: "Pelvic Tilt", status: "done" },
+        { day: 2, label: "Knee-to-Chest Stretch", status: "pending" },
+        { day: 3, label: "Bridge Exercise", status: "pending" },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Accordion items={accordionItems} />
+    </div>
+  );
 }
