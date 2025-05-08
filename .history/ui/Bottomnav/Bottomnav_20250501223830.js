158da3143763f04d9f6e92c911a64c73efc2e459
patcha-d3 <psricome@my.bcit.ca>
2025-05-08 11:37:42 -0700

    updated activeIcons to be green

--- .history/ui/Bottomnav/Bottomnav_20250501223830.js
+++ .history/ui/Bottomnav/Bottomnav_20250501223830.js
@@ -4,20 +4,14 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./Bottomnav.module.css";
-
-// Import SVGs as React components
-import HomeIcon from '../../public/icons/home.svg';
-import RediagnoseIcon from '../../public/icons/rediagnose.svg';
-import ProgressIcon from '../../public/icons/progress.svg';
-import BlogIcon from '../../public/icons/blog.svg';
-import SettingsIcon from '../../public/icons/settings.svg';
+import Image from "next/image";
 
 const menuItems = [
-  { label: "Home", icon: HomeIcon, path: "/" },
-  { label: "Re-Diagnose", icon: RediagnoseIcon, path: "/re-diagnose" },
-  { label: "Progress", icon: ProgressIcon, path: "/progress" },
-  { label: "Blog", icon: BlogIcon, path: "/blog" },
-  { label: "Settings", icon: SettingsIcon, path: "/settings" },
+  { label: "Home", icon: "/icons/home.svg", path: "/" },
+  { label: "Re-Diagnose", icon: "/icons/rediagnose.svg", path: "/re-diagnose" },
+  { label: "Progress", icon: "/icons/progress.svg", path: "/progress" },
+  { label: "Blog", icon: "/icons/blog.svg", path: "/blog" },
+  { label: "Settings", icon: "/icons/settings.svg", path: "/settings" },
 ];
 
 export default function MobileNavbar() {
@@ -27,14 +21,21 @@ export default function MobileNavbar() {
     <nav className={styles.navbar}>
       {menuItems.map((item, index) => {
         const isActive = pathname === item.path;
-        const IconComponent = item.icon;
+
         return (
           <Link href={item.path} key={index} className={styles.link}>
             <div className={styles.button}>
-              <IconComponent
-                className={`${styles.icon} ${isActive ? styles.iconActive : ""}`}
-                aria-label={item.label}
-              />
+              <div className={styles.icon}>
+                <Image
+                  src={item.icon}
+                  alt={item.label}
+                  width={20}
+                  height={20}
+                  style={{
+                    filter: isActive ? "brightness(0) invert(1)" : "none",
+                  }}
+                />
+              </div>
               <span
                 className={`${styles.label} ${
                   isActive ? styles.labelActive : styles.labelInactive
