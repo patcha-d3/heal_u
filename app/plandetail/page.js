c960309ae86b8ef45acd3d60ffe4006e3a8edabb
HyunJunggggg <hyunjung9219@gmail.com>
2025-05-08 20:43:35 -0700

    hide the bottom nav

--- app/plandetail/page.js
+++ app/plandetail/page.js
@@ -1,7 +1,21 @@
 import VideoComponent from "../../ui/Video-component/Video-component";
+import ButtonBack from "../../ui/Button-back/Button-back";
+import SurveyProgress from "../../ui/SurveyProgress/SurveyProgress";
 
 export default function SomePage() {
-  return <VideoComponent />;
+  const currentStep = 1; // or get this from props/state
+  const totalSteps = 8;  // or get this from props/state
+
+  return (
+    <div className="relative h-full">
+      <ButtonBack className="absolute top-[48px] left-[11px]" />
+      <main className="h-full flex flex-col px-4 py-[18px]">
+        <SurveyProgress
+          currentStep={currentStep}
+          totalSteps={totalSteps}
+        />
+        <VideoComponent />
+      </main>
+    </div>
+  );
 }
-
-
