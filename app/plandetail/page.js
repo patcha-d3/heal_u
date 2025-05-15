ce07560c4fd68023174db78bc1b774120433c113
HyunJunggggg <hyunjung9219@gmail.com>
2025-05-15 12:01:12 -0700

    video progress

--- app/plandetail/page.js
+++ app/plandetail/page.js
@@ -1,19 +1,17 @@
 import VideoComponent from "../../ui/Video-component/Video-component";
 import ButtonBack from "../../ui/Button-back/Button-back";
-import SurveyProgress from "../../ui/SurveyProgress/SurveyProgress";
+import VideoProgress from "../../ui/VideoProgress/Videoprogress";
+import SurveyProgress from "@/ui/SurveyProgress/SurveyProgress";
 
 export default function SomePage() {
   const currentStep = 1; // or get this from props/state
-  const totalSteps = 8;  // or get this from props/state
+  const totalSteps = 8; // or get this from props/state
 
   return (
     <div className="relative h-full">
       <ButtonBack className="absolute top-[48px] left-[11px]" />
       <main className="h-full flex flex-col px-4 py-[18px]">
-        <SurveyProgress
-          currentStep={currentStep}
-          totalSteps={totalSteps}
-        />
+        <VideoProgress currentStep={6} totalSteps={7} />
         <VideoComponent />
       </main>
     </div>
