781a1b4b8d681a673ae93a050fed6be4f666ec09
HyunJunggggg <hyunjung9219@gmail.com>
2025-05-15 12:18:06 -0700

    change progress bar status

--- app/plandetail/page.js
+++ app/plandetail/page.js
@@ -1,17 +1,16 @@
 import VideoComponent from "../../ui/Video-component/Video-component";
 import ButtonBack from "../../ui/Button-back/Button-back";
 import VideoProgress from "../../ui/VideoProgress/Videoprogress";
-import SurveyProgress from "@/ui/SurveyProgress/SurveyProgress";
 
 export default function SomePage() {
   const currentStep = 1; // or get this from props/state
-  const totalSteps = 8; // or get this from props/state
+  const totalSteps = 6; // or get this from props/state
 
   return (
     <div className="relative h-full">
       <ButtonBack className="absolute top-[48px] left-[11px]" />
       <main className="h-full flex flex-col px-4 py-[18px]">
-        <VideoProgress currentStep={6} totalSteps={7} />
+        <VideoProgress currentStep={6} totalSteps={6} />
         <VideoComponent />
       </main>
     </div>
